507fff0233e2fc3c14cce29eac5798455a4acb56
Rafael Ferreira Gomes <ferreira.rafael862@gmail.com>
2022-08-06 18:12:53 -0300

    started the process in the progamer

---
+++ scripts/CalcController.js
@@ -0,0 +1,14 @@
+class CalcController {
+
+    //classe tem atributos e métodos
+     constructor(){
+        this._displayCalc = "0"; //
+
+     }
+     get displayCalc(){
+        return this._displayCalc;
+     }
+
+     get
+
+}
