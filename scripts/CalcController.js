5c03bda1ff3426eaeead60d3600f3058b16dd0f1
Rafael Ferreira Gomes <ferreira.rafael862@gmail.com>
2022-08-09 21:27:44 -0300

    o valor correto já está sendo impresso no console

--- scripts/CalcController.js
+++ scripts/CalcController.js
@@ -1,14 +1,113 @@
 class CalcController {
 
     //classe tem atributos e métodos
-     constructor(){
-        this._displayCalc = "0"; //
+
+     constructor(){ //tudo do construtor é executado automaticamente
+
+         this._displayCalcEl = document.querySelector("#display"); //foi colocado o elemento referido a direita dentro da variável a esquerda
+         this.initialize(); //metodo -- é igual função --
+         this.initButtonsEvents();//chamando função no construtor
+         this._operation = []; 
 
      }
+
+     initialize(){ //metodo
+
+     }
+
+     addEventListenerAll(element, events, fn){ //metodo
+
+      events.split(' ').forEach(event => {
+         element.addEventListener(event, fn, false);
+      });
+
+     }
+
+     clearAll(){ //metodo
+
+
+
+     }
+
+     execBtn(value){
+
+      switch (value){
+
+         case 'C':
+            this.clearAll();
+            break;
+
+         case 'CE':
+            this.clearAll();
+            break;
+
+         case '-':
+            this.clearAll();
+            break; 
+            
+         case '+':
+            this.clearAll();
+            break;
+            
+         case '/':
+            this.clearAll();
+            break;
+
+         case '*':
+            this.clearAll();
+            break;
+
+         case '=':
+            this.clearAll();
+            break;
+
+         case '%':
+            this.clearAll();
+            break;
+
+         case '':
+            this.clearAll();
+            break;
+
+            default:
+               this.setError();
+               break;
+      }
+     }
+
+     initButtonsEvents(){
+
+      let buttons = document.querySelectorAll(".row > button");  
+
+      buttons.forEach((btn, index)=>{
+
+         this.addEventListenerAll(btn, "click drag", e =>{
+           let textBtn = btn.textContent;
+
+           this.execBtn();
+         });
+
+         this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+
+            btn.getElementsByClassName.cursor = "pointer";
+
+         });
+
+      });
+   }
+
+
+
      get displayCalc(){
-        return this._displayCalc;
+
+        return this._displayCalcEl.innerHTML; //retorna o valor que está setado acima
+
      }
 
-     get
+     set displayCalc(valor){
+
+         this._displayCalcEl.innerHTML = valor; //insere no html o valor que foi recebido
+
+     }
 
 }
