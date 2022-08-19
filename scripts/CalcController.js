c911e33e6a5d7a50d1e4da893a96f1045b824757
Rafael Ferreira Gomes <ferreira.rafael862@gmail.com>
2022-08-18 21:39:37 -0300

    starting over

--- scripts/CalcController.js
+++ scripts/CalcController.js
@@ -4,124 +4,20 @@ class CalcController {
 
      constructor(){ //tudo do construtor é executado automaticamente
 
-         this._displayCalcEl = document.querySelector("#display"); //foi colocado o elemento referido a direita dentro da variável a esquerda
-         this.initialize(); //metodo -- é igual função --
-         this.initButtonsEvents();//chamando função no construtor
-         this._operation = []; 
+         this._displayCalc = "0";
 
      }
 
-     initialize(){ //metodo
+     get displayCalc(){ // get é para registrar
+
+      return this._displayCalc;
 
      }
 
-     addEventListenerAll(element, events, fn){ //metodo
-
-      events.split(' ').forEach(event => {
-         element.addEventListener(event, fn, false);
-      });
-
+     set displayCalc(valor){ //set é para definir uum novo valor
+      this.displayCalc = valor;
      }
 
-     clearAll(){ //metodo
-      this._operation = [];
-     }
 
-     clearEntry(){ //metodo //teste commit
-      this._operation.pop();
-     }
 
-     addOperation (value){ //metodo
-      this._operation.push(value);
-      console.log(this._operation);
-     }
-
-     setError(){ //metodo
-      this.displayCalc = "Error";
-     }
-
-     execBtn(value){
-
-      switch (value){
-
-         case 'C':
-            this.clearAll();
-            break;
-
-         case 'CE':
-            this.clearEntry();
-            break;
-
-         case '-':
-            
-            break; 
-            
-         case '+':
-            
-            break;
-            
-         case '/':
-            
-            break;
-
-         case '*':
-            
-            break;
-
-         case '=':
-            
-            break;
-
-         case '%':
-            
-            break;
-
-         case '0':
-         case '1':
-         case '2':
-         case '3':
-         case '4':
-         case '5':
-         case '6':
-         case '7':
-         case '8':
-         case '9': 
-            this.displayCalc.addEventListener("click", )
-            //this.addOperation(parseInt(value)); 
-            break;
-
-         default:
-            this.setError();
-            break;
-      }
-     }
-
-     initButtonsEvents(){
-
-      let buttons = document.querySelectorAll(".row > #btn1");  
-
-      buttons.forEach(btn=>{
-
-         btn.addEventListenerAll("click", e => {
-           console.log(e);
-         });
-
-         
-      });
    }
-
-
-
-     get displayCalc(){
-
-        return this._displayCalcEl.innerHTML; //retorna o valor que está setado acima
-
-     }
-
-     set displayCalc(valor){
-
-         this._displayCalcEl.innerHTML = valor; //insere no html o valor que foi recebido
-
-     }
-    
-}
